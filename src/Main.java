6e001f78a77fee48841eba480fd78e865d237631
JhaymesBach <jhaymesbachmann@gmail.com>
2024-08-21 20:47:49 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer IdadePessoa = 20;
+        String nomePessoa = "taffezinho";
+
+
+        /* *
+        * operadores logicos
+        *  > maior
+        *  >= maior igual
+        *  < menor
+        *  <= menor igual
+        *  == é igual
+        *  != diferente
+        *  equal compara textos
+        *
+        * */
+        if(IdadePessoa > 21 ){
+            System.out.println("é maior que 21");
+        }else {
+            System.out.println("nao é maior");
+        }
+
+
     }
 }
